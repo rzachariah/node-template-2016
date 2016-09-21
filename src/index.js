6535af3dae730adf96f7aafcd163ed3b34fd8d8c
Ranjith Zachariah <ranjith.zachariah@gmail.com>
2016-09-21 07:58:03 -0400

    Move source to src. Watch all src changes

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello, world!'));
 
-app.listen(port, () => console.log(`GraphQL server running at http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
